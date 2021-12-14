364ef64d4675bc7da6ddfe745c80dafc1389cef1
Alex Kornitzer <alex.kornitzer@f-secure.com>
2021-12-14 18:16:51 +0000

    fix: remove yml prefixing

--- tests/common.rs
+++ tests/common.rs
@@ -4,13 +4,8 @@ use std::path::Path;
 use anyhow::Error;
 
 pub fn load_file(prefix: &str, name: &str) -> Result<String, Error> {
-    let rule = if name.ends_with(".yml") {
-        name.to_owned()
-    } else {
-        format!("{}.yml", name)
-    };
     let root = env!("CARGO_MANIFEST_DIR");
-    let path = Path::new(root).join(prefix).join(rule);
+    let path = Path::new(root).join(prefix).join(name);
     let contents = fs::read_to_string(path).expect("failed to read rule");
     Ok(contents)
 }
