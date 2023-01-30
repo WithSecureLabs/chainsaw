580d85e918f15276329b4beb6a3071f63719f0bb
Markus Tuominen <markus@tuominen.net>
2023-01-30 15:59:10 +0200

    Initial amcache parsing

--- src/file/hve.rs
+++ src/file/hve.rs
@@ -6,12 +6,50 @@ use notatin::{
     parser_builder::ParserBuilder,
 };
 use serde_json::Value as Json;
-use std::path::Path;
+use std::{path::Path, collections::HashMap};
 
 pub type Hve = Json;
 
 pub struct Parser {
     pub inner: HveParser,
+}
+
+#[derive(Debug)]
+pub struct InventoryApplicationFileArtifact {
+    pub program_id: String,
+    pub file_id: String,
+    pub path: String,
+    pub sha1_hash: String,
+    pub link_date: Option<NaiveDateTime>,
+    pub last_modified_ts: NaiveDateTime,
+}
+
+#[derive(Debug)]
+pub struct InventoryApplicationArtifact {
+    pub program_id: String,
+    pub program_name: String,
+    pub install_date: Option<NaiveDateTime>,
+}
+#[derive(Debug)]
+pub struct ProgramArtifact {
+    pub program_id: String,
+    pub application_artifact: Option<InventoryApplicationArtifact>,
+    pub files: Vec<InventoryApplicationFileArtifact>,
+}
+
+impl ProgramArtifact {
+    pub fn new(program_id: &str) -> Self {
+        Self {
+            program_id: program_id.to_string(),
+            application_artifact: None,
+            files: Vec::new(),
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct AmcacheArtifact {
+    pub programs: HashMap<String, ProgramArtifact>,
 }
 
 impl Parser {
@@ -36,7 +74,7 @@ impl Parser {
             })
     }
 
-    pub fn parse_amcache(&mut self) -> Result<CellKeyNode, Error> {
+    pub fn parse_amcache(&mut self) -> anyhow::Result<AmcacheArtifact> {
         fn string_value_from_key(key: &CellKeyNode, value_name: &str) -> Result<String, Error> {
             let Some(key_value) = key.get_value(value_name) else {
                 return Err(anyhow::anyhow!(
@@ -47,32 +85,8 @@ impl Parser {
             Ok(key_value.get_content().0.to_string())
         }
 
-        // Parse registry key InventoryApplicationFile
-        let key_inventory_application_file = self
-            .inner
-            .get_key("Root\\InventoryApplicationFile", false)?;
-        let Some(mut node_inventory_application_file) = key_inventory_application_file else {
-            return Err(anyhow::anyhow!("Could not find InventoryApplicationFile key!"));
-        };
-        let subkeys = node_inventory_application_file
-            .read_sub_keys(&mut self.inner);
-        for key in subkeys {
-            let program_id = string_value_from_key(&key, "ProgramId")?;
-            let file_id = string_value_from_key(&key, "FileId")?;
-            let path = string_value_from_key(&key, "LowerCaseLongPath")?;
-            let link_date = string_value_from_key(&key, "LinkDate")?;
+        let mut programs: HashMap<String, ProgramArtifact> = HashMap::new();
 
-            let sha1_hash = &file_id[4..];
-
-            let Some(last_modified_ts)
-                = win32_ts_to_date_time(key.detail.last_key_written_date_and_time()) else {
-                return Err(anyhow::anyhow!("Could not parse timestamp!"));
-            };
-            println!("{}", last_modified_ts);
-            println!("{}", link_date);
-            println!("{}", path);
-            println!("{}\n", sha1_hash);
-        }
         // Parse registry key InventoryApplication
         let key_inventory_application_file = self
             .inner
@@ -83,17 +97,82 @@ impl Parser {
         let subkeys = node_inventory_application.read_sub_keys(&mut self.inner);
         for key in subkeys {
             let program_id = key.key_name.clone();
+            let program_name = string_value_from_key(&key, "Name")?;
             let install_date = string_value_from_key(&key, "InstallDate")?;
+
+            let install_date = if !install_date.is_empty() {
+                Some(win_reg_str_ts_to_date_time(install_date.as_str())?)
+            } else {
+                None
+            };
+
+            let mut program_artifact = ProgramArtifact::new(&program_id);
+
+            let app_artifact = InventoryApplicationArtifact {
+                program_id,
+                program_name,
+                install_date,
+            };
+
+            program_artifact.application_artifact = Some(app_artifact);
+
+            programs.insert(program_artifact.program_id.clone(), program_artifact);
         }
 
-        //TODO: link executables using program id
-        Ok(node_inventory_application)
+        // Parse registry key InventoryApplicationFile
+        let key_inventory_application_file = self
+            .inner
+            .get_key("Root\\InventoryApplicationFile", false)?;
+        let Some(mut node_inventory_application_file) = key_inventory_application_file else {
+            return Err(anyhow::anyhow!("Could not find InventoryApplicationFile key!"));
+        };
+        let subkeys = node_inventory_application_file.read_sub_keys(&mut self.inner);
+        for key in subkeys {
+            let program_id = string_value_from_key(&key, "ProgramId")?;
+            let file_id = string_value_from_key(&key, "FileId")?;
+            let path = string_value_from_key(&key, "LowerCaseLongPath")?;
+            let link_date_str = string_value_from_key(&key, "LinkDate")?;
+            let link_date = if !link_date_str.is_empty() {
+                Some(win_reg_str_ts_to_date_time(link_date_str.as_str())?)
+            } else {
+                None
+            };
+
+            let sha1_hash = String::from(&file_id[4..]);
+
+            let last_modified_ts = win32_ts_to_datetime(key.detail.last_key_written_date_and_time())
+                .ok_or(anyhow::anyhow!("Could not parse timestamp!"))?;
+            let file_artifact = InventoryApplicationFileArtifact {
+                program_id,
+                file_id,
+                path,
+                sha1_hash,
+                link_date,
+                last_modified_ts,
+            };
+            match programs.get_mut(&file_artifact.program_id) {
+                Some(program) => {
+                    program.files.push(file_artifact);
+                },
+                None => {
+                    let mut program = ProgramArtifact::new(&file_artifact.program_id);
+                    program.files.push(file_artifact);
+                    programs.insert(program.program_id.clone(), program);
+                }
+            }
+        }
+
+        Ok(AmcacheArtifact { programs })
     }
 }
 
-fn win32_ts_to_date_time(ts_win32: u64) -> Option<NaiveDateTime> {
+fn win32_ts_to_datetime(ts_win32: u64) -> Option<NaiveDateTime> {
     let ts_unix = ((ts_win32 / 10_000) - 11644473600000) as i64;
     chrono::prelude::NaiveDateTime::from_timestamp_millis(ts_unix)
+}
+
+fn win_reg_str_ts_to_date_time(ts_str: &str) -> anyhow::Result<NaiveDateTime> {
+    Ok(NaiveDateTime::parse_from_str(ts_str, "%m/%d/%Y %H:%M:%S")?)
 }
 
 #[cfg(test)]
@@ -105,8 +184,8 @@ mod tests {
         let mut parser = Parser::load(&Path::new(
             "/mnt/hgfs/vm_shared/Module 5 - Disk/cache/amcache/Amcache.hve",
         ))?;
-        let node = parser.parse_amcache()?;
-        // println!("{:#?}", node);
+        let artifact_map = parser.parse_amcache()?;
+        println!("{:#?}", artifact_map);
         Ok(())
     }
 }
