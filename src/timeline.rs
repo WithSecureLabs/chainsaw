e67a4b79f6e04e67719d1094fd67f11fdc23d600
Markus Tuominen <markus@tuominen.net>
2023-02-06 10:00:50 +0200

    Timestamp range refinement

--- src/timeline.rs
+++ src/timeline.rs
@@ -12,7 +12,7 @@ use crate::file::hve::{
 };
 
 #[derive(Debug, Clone)]
-enum TimeLineTimestamp {
+enum TimelineTimestamp {
     Exact(DateTime<Utc>),
     Range(DateTime<Utc>, DateTime<Utc>), // (from, to)
     RangeStart(DateTime<Utc>),
@@ -23,7 +23,7 @@ enum TimeLineTimestamp {
 struct TimelineEntity {
     shimcache_entry: ShimCacheEntry,
     amcache_entry: Option<FileArtifact>,
-    timestamp: Option<TimeLineTimestamp>,
+    timestamp: Option<TimelineTimestamp>,
     amcache_ts_match: bool,
 }
 
@@ -69,7 +69,7 @@ fn amcache_shimcache_timeline(
             };
             if pattern_matches {
                 if let Some(ts) = entity.shimcache_entry.last_modified_ts {
-                    entity.timestamp = Some(TimeLineTimestamp::Exact(ts.clone()));
+                    entity.timestamp = Some(TimelineTimestamp::Exact(ts.clone()));
                     match_indices.push(i);
                 }
                 break;
@@ -81,39 +81,51 @@ fn amcache_shimcache_timeline(
         return Ok(None);
     }
 
+    fn extract_ts_from_entity(entity: &TimelineEntity) -> DateTime<Utc> {
+        match entity.timestamp {
+            Some(TimelineTimestamp::Exact(timestamp)) => timestamp,
+            _ => panic!("Provided entities should only have exact timestamps!"),
+        }
+    }
+
+    fn set_timestamp_ranges(range_indices: &Vec<usize>, timeline_entities: &mut Vec<TimelineEntity>) {
+        // Set timestamp ranges for timeline entities based on shimcache entry order
+        let first_index = *range_indices.first().unwrap();
+        if first_index > 0 {
+            let entity = &timeline_entities[first_index];
+            let ts = TimelineTimestamp::RangeStart(extract_ts_from_entity(entity));
+            let first_range = 0usize..first_index;
+            for i in first_range {
+                timeline_entities[i].timestamp = Some(ts.clone());
+            }
+        }
+        for pair in range_indices.windows(2) {
+            let start_i = pair[0];
+            let end_i = pair[1];
+            let start_entity = &timeline_entities[start_i];
+            let end_entity = &timeline_entities[end_i];
+            let ts = TimelineTimestamp::Range(
+                extract_ts_from_entity(end_entity),
+                extract_ts_from_entity(start_entity)
+            );
+            let range = start_i+1..end_i;
+            for i in range {
+                timeline_entities[i].timestamp = Some(ts.clone());
+            }
+        }
+        let last_index = *range_indices.last().unwrap();
+        if last_index+1 < timeline_entities.len() {
+            let entity = &timeline_entities[last_index];
+            let ts = TimelineTimestamp::RangeEnd(extract_ts_from_entity(entity));
+            let last_range = last_index+1..timeline_entities.len();
+            for i in last_range {
+                timeline_entities[i].timestamp = Some(ts.clone());
+            }
+        }
+    }
+
     // Set timestamp ranges for timeline entities based on shimcache entry order
-    let first_index = *match_indices.first().unwrap();
-    if first_index > 0 {
-        let entity = &timeline_entities[first_index];
-        let ts = TimeLineTimestamp::RangeStart(entity.shimcache_entry.last_modified_ts.unwrap());
-        let first_range = 0usize..first_index;
-        for i in first_range {
-            timeline_entities[i].timestamp = Some(ts.clone());
-        }
-    }
-    for pair in match_indices.windows(2) {
-        let start_i = pair[0];
-        let end_i = pair[1];
-        let start_entity = &timeline_entities[start_i];
-        let end_entity = &timeline_entities[end_i];
-        let ts = TimeLineTimestamp::Range(
-            end_entity.shimcache_entry.last_modified_ts.unwrap(),
-            start_entity.shimcache_entry.last_modified_ts.unwrap()
-        );
-        let range = start_i+1..end_i;
-        for i in range {
-            timeline_entities[i].timestamp = Some(ts.clone());
-        }
-    }
-    let last_index = *match_indices.last().unwrap();
-    if last_index+1 < timeline_entities.len() {
-        let entity = &timeline_entities[last_index];
-        let ts = TimeLineTimestamp::RangeEnd(entity.shimcache_entry.last_modified_ts.unwrap());
-        let last_range = last_index+1..timeline_entities.len();
-        for i in last_range {
-            timeline_entities[i].timestamp = Some(ts.clone());
-        }
-    }
+    set_timestamp_ranges(&match_indices, &mut timeline_entities);
 
     // Match shimcache and amcache entries and 
     // check if amcache timestamp matches timeline timestamp range
@@ -123,11 +135,11 @@ fn amcache_shimcache_timeline(
                 ProgramType::Executable { path } => {
                     if file.path == path.to_lowercase() {
                         entity.amcache_entry = Some(file.clone());
-                        if let Some(TimeLineTimestamp::Range(from, to)) = entity.timestamp {
+                        if let Some(TimelineTimestamp::Range(from, to)) = entity.timestamp {
                             let amcache_ts = file.last_modified_ts;
                             if from < amcache_ts && amcache_ts < to {
                                 entity.amcache_ts_match = true;
-                                entity.timestamp = Some(TimeLineTimestamp::Exact(amcache_ts));
+                                entity.timestamp = Some(TimelineTimestamp::Exact(amcache_ts));
                             }
                         }
                     }
@@ -138,7 +150,14 @@ fn amcache_shimcache_timeline(
         }
     }
 
-    //TODO: refine timestamp ranges further by using amcache matched exact timestamps
+    // Refine timestamp ranges based on amcache matches
+    for (i, entity) in timeline_entities.iter_mut().enumerate() {
+        if entity.amcache_ts_match {
+            match_indices.push(i);
+        }
+    }
+    match_indices.sort();
+    set_timestamp_ranges(&match_indices, &mut timeline_entities);
 
     Ok(Some(timeline_entities))
 }
@@ -153,6 +172,7 @@ mod tests {
             "/mnt/hgfs/vm_shared/win10_vm_hives/am/Amcache.hve",
             "/mnt/hgfs/vm_shared/win10_vm_hives/shim/SYSTEM"
         )?.unwrap();
+        println!("shimcache_position;amcache_timestamp_match;timeline_ts;program");
         for entity in timeline {
             println!("{};{:?};{:?};{:?}", entity.shimcache_entry.cache_entry_position, entity.amcache_ts_match, entity.timestamp, entity.shimcache_entry.program);
         }
