ad7a279b9159753ee604ff238f7e722a55bcc9c3
unrooted <konrad.root.klawikowski@gmail.com>
2024-01-10 16:10:53 +0100

    add nix flake

---
+++ flake.nix
@@ -0,0 +1,48 @@
+{
+  description = "Rapidly Search and Hunt through Windows Forensic Artefacts";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+  outputs = inputs: let
+    systems = ["x86_64-linux"];
+    genSystems = inputs.nixpkgs.lib.genAttrs systems;
+  in {
+    devShells = genSystems (system: let
+      pkgs = inputs.nixpkgs.legacyPackages.${system};
+    in {
+      default = pkgs.mkShell {
+        inputsFrom = [inputs.self.packages.${system}.chainsaw];
+        packages = with pkgs; [
+          cargo
+          pre-commit
+          rust-analyzer
+          rustc
+          rustfmt
+          vscode-extensions.llvm-org.lldb-vscode
+        ];
+
+        RUST_SRC_PATH = pkgs.rustPlatform.rustLibSrc;
+      };
+    });
+
+    packages = genSystems (system: let
+      pkgs = inputs.nixpkgs.legacyPackages.${system};
+
+    in {
+      chainsaw = pkgs.rustPlatform.buildRustPackage {
+        pname = "chainsaw";
+        version = "2.8.1";
+
+        src = ./.;
+
+        doCheck = false;
+
+        cargoLock.lockFile = ./Cargo.lock;
+      };
+      default = inputs.self.packages.${system}.chainsaw;
+    });
+
+    formatter = genSystems (system: inputs.nixpkgs.legacyPackages.${system}.alejandra);
+  };
+}
+
