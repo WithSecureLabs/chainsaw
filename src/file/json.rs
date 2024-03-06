e47f9ce066960795675a2e45e491d0e52016c05f
James D <8268953+FranticTyping@users.noreply.github.com>
2024-03-06 12:49:58 -0800

    feat: match_all option for regex patterns (#163)

--- src/file/json.rs
+++ src/file/json.rs
@@ -53,8 +53,17 @@ impl Iterator for ParserIter {
 }
 
 impl Searchable for Json {
-    fn matches(&self, regex: &RegexSet) -> bool {
-        regex.is_match(&self.to_string().replace(r"\\", r"\"))
+    fn matches(&self, regex: &RegexSet, match_all: &bool) -> bool {
+        if *match_all {
+            regex
+                .matches(&self.to_string().replace(r"\\", r"\"))
+                .into_iter()
+                .collect::<Vec<_>>()
+                .len()
+                == regex.len()
+        } else {
+            regex.is_match(&self.to_string().replace(r"\\", r"\"))
+        }
     }
 }
 
